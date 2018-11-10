173c887d88192b4e1c559d1d24f2f2419daacdec
sbprotected <44929223+sbprotected@users.noreply.github.com>
2018-11-10 16:56:59 -0500

    updated Number class
    added __pow__ support, fixed fraction reducing, added comparison operator support for >, <, and ==.

--- Number.py
+++ Number.py
@@ -1,6 +1,10 @@
 from math import gcd;
 
 
+def nth_root(n, index):
+    return n ** (1 / index);
+
+
 class Number():
 
     """ Object for representing numerical values in an expression """
@@ -10,7 +14,7 @@ class Number():
             self.numerator, self.denominator = float(value).as_integer_ratio();
         elif isinstance(value, tuple):
             if len(value) == 2 and value[1] != 0:
-                self.numerator, self.denominator = value;
+                self.numerator, self.denominator = map(int, value);
             else:
                 raise ValueError("tuple must be of length 2 and the denominator cannot be 0.");
         else:
@@ -19,6 +23,9 @@ class Number():
 
     def __repr__(self):
         return "Number( {} / {} )".format(self.numerator, self.denominator);
+
+    def __neg__(self):
+        return Number(-1) * self;
 
     def __add__(self, other):
         numerator = self.numerator + other.numerator;
@@ -40,6 +47,19 @@ class Number():
         denominator = self.denominator * other.numerator;
         return Number((numerator, denominator));
 
+    def __pow__(self, other):
+        return Number((nth_root(self.numerator, other.denominator) ** other.numerator,
+                      nth_root(self.denominator, other.denominator) ** other.numerator));
+
+    def __eq__(self, other):
+        return (self.numerator, self.denominator) == (other.numerator, other.denominator);
+
+    def __lt__(self, other):
+        return self.as_decimal()  < other.as_decimal();
+
+    def __gt__(self, other):
+        return self.as_decimal() > other.as_decimal();
+
     def as_decimal(self):
         """ return the value in decimal form. """
         return self.numerator / self.denominator;
@@ -47,5 +67,5 @@ class Number():
     def reduce(self):
         """ Simplify the fraction """
         common_divisor = gcd(self.numerator, self.denominator);
-        self.numerator /= common_divisor;
-        self.denominator /= common_divisor;
+        self.numerator //= common_divisor;
+        self.denominator //= common_divisor;
